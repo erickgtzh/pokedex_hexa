e11ed66741762d6e79d306479b697372fe3f80af
Erick <0193662@up.edu.mx>
2023-09-28 14:08:44 -0600

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import NavigationConfig from './src/navigation/NavigationConfig';
+
+const App = () => {
+  return <NavigationConfig />;
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,2 @@
+# pokedex_hexa
+ proyecto creado para hexalud utilizando react native
